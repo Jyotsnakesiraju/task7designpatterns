475591f73951c60a1030137dea947558559f0ffd
Jyotsnakesiraju <jyotsnasri1@gmail.com>
2020-07-21 20:00:48 +0530

    commit message

---
+++ src/Mealb.java
@@ -0,0 +1,9 @@
+
+public class Mealb{
+	public Meal VegMeal(){
+      Meal m = new Meal();
+      m.addItem(new Vegburger());
+      m.addItem(new Thumsup());
+      return m;
+   }   
+}
---
+++ src/Thumsup.java
@@ -0,0 +1,13 @@
+
+public class Thumsup extends Softdrink {
+
+   @Override
+   public float price() {
+      return 30.0f;
+   }
+
+   @Override
+   public String name() {
+      return "Thumsup";
+   }
+}

471a4f9db4ba1017c14f99c58cdcbc829338f030
Jyotsnakesiraju <jyotsnasri1@gmail.com>
2020-07-21 19:25:41 +0530

    commit message

---
+++ src/AbstractFactoryPattern.java
@@ -0,0 +1,4 @@
+
+public abstract class AbstractFactoryPattern {
+	abstract Shape getShape(String shapeType) ;
+}
---
+++ src/Producer.java
@@ -0,0 +1,10 @@
+
+public class Producer {
+	public static AbstractFactoryPattern getFactory(boolean rounded){   
+	      if(rounded){
+	         return new RoundedShape();         
+	      }else{
+	         return new ShapeFactory();
+	      }
+	   }
+}
